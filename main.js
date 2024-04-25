9f9bdba10a4481371fe5cb592ca0e18b4e160a2d
armeen912 <164738082+armeen912@users.noreply.github.com>
2024-04-25 16:28:33 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+#! //usr/bin/env node
+import inquirer from "inquirer";
+let todo = [];
+let addTask = await inquirer.prompt([
+    {
+        name: "todo",
+        type: "input",
+        message: "What would you like  to add in you Todos ?",
+    },
+    {
+        name: "addMore",
+        type: "comfirm",
+        message: "Would  you like to add more in your todos ?",
+        default: "true ",
+    },
+]);
+todo.push(addTask);
+console.log(addTask);
